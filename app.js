d9dd743a870f709459022f313f4952325940f43a
Fardeen Khan <khanfardeen9983@gmail.com>
2019-04-15 23:08:56 +0530

    Sum function created and exported as module

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
 console.log("hello there");
+let tutorial = require("./tutorial");
+console.log("Sum of 4 + 5 : " + tutorial(4,5));
---
+++ tutorial.js
@@ -0,0 +1,2 @@
+const sum = (n1,n2) => n1+n2;
+module.exports = sum;
