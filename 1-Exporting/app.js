820e602cc5ce6f1c7b30e89ad097048d7752ddab
Fardeen Khan <khanfardeen9983@gmail.com>
2019-04-18 13:26:47 +0530

    Starting with Event Emitters

--- 1-Exporting/app.js
+++ 1-Exporting/app.js
@@ -1,3 +1,4 @@
+//1.0 Basic Console API and Exporting
 console.log("hello there");
 let tutorial = require("./tutorial");
 console.log("Sum of 4 + 5 : " + tutorial.sum(4,5) +"\n" + "PI : " + tutorial.PI);
--- 1-Exporting/tutorial.js
+++ 1-Exporting/tutorial.js
@@ -1,3 +1,5 @@
+// Some fields and classes for exporting into a module
+
 const sum = (n1,n2) => n1+n2;
 const PI = 3.14;
 class SomeMathObject {
