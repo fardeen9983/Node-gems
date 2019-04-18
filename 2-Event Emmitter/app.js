fa9327830e124616380be1de6a0d043abf94283b
Fardeen Khan <khanfardeen9983@gmail.com>
2019-04-18 13:39:07 +0530

    Add listeners to default EventEmitter objects

--- 2-Event Emmitter/app.js
+++ 2-Event Emmitter/app.js
@@ -1,2 +1,24 @@
 // 2.0 Event Emitter
 
+//Import events package
+const EventEmiiter = require("events");
+//Get an EventEmitter Object
+const eventEmitter = new EventEmiiter();
+
+/**
+ * Attach a listenere to the EventEmitter Object using on method
+ * arg0 => String/Number : event identifier 
+ * arg1 => a method to be called when the event occurs (anonymous method used here)
+ */
+eventEmitter.on("tutorial",()=>{
+    console.log("Event name : tutorial");
+});
+//Emit a 'tutorial' event
+eventEmitter.emit("tutorial");
+
+//Passing arguments to event listener
+eventEmitter.on("sum",(n1,n2)=>{
+    console.log("Sum : " + (n1+n2));
+});
+//Emit a 'sum' event
+eventEmitter.emit("sum",2,3);
