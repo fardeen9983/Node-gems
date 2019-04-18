fd778916d10bc9cfdb172e7da8ddc95fc0dd40be
Fardeen Khan <khanfardeen9983@gmail.com>
2019-04-18 17:24:01 +0530

    Read file contents

--- 4-File System/app.js
+++ 4-File System/app.js
@@ -15,3 +15,18 @@ fs.writeFile('example.txt', "Hello Moto.\nGoogle Assistant.\nAlexa\nSiri",
     else 
         console.log("File operation completed");
 });
+
+/**
+ * Read from a file
+ * method : readFile
+ * arg0 : String -> File path|URL|Buffer
+ * arg1 : String -> Encoding type | default: Buffer 
+ * arg2 : callback(error,file) -> handle errors and get file content 
+ */
+fs.readFile('example.txt',"UTF-8",(err,file)=>{
+    if(err)
+        console.log("Error occured");
+    else 
+        console.log(file);
+});
+ 
