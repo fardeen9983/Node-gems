3f2c6377a3d8aac01c91035ff742a0494c264b89
Fardeen Khan <khanfardeen9983@gmail.com>
2019-04-18 13:18:47 +0530

    Few other properties exported as Object form tutorial.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("hello there");
 let tutorial = require("./tutorial");
-console.log("Sum of 4 + 5 : " + tutorial(4,5));
+console.log("Sum of 4 + 5 : " + tutorial.sum(4,5) +"\n" + "PI : " + tutorial.PI);
+new tutorial.SomeMathObject();
--- tutorial.js
+++ tutorial.js
@@ -1,2 +1,9 @@
 const sum = (n1,n2) => n1+n2;
-module.exports = sum;
+const PI = 3.14;
+class SomeMathObject {
+    constructor(){
+        console.log("Objkect created");
+    }
+}
+
+module.exports = {sum : sum,PI : PI, SomeMathObject}
